d1d9cfc3f058aa37f9eb85fcefdb2b666860e622
trincellshelvin <trincellshelvin@gmail.com>
2024-08-13 04:34:37 +0000

    stopped at creating play and feed method, needs finishing once rewatch class video

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const pet ={
+const pet = {
     name: 'Pheobe Cinnamon Sunrise',
     species: 'Dog',
     breed: 'Miniture Pitbull Terrier',
-    happiness: 50
-};
+    happiness: 50,
+
+    playwithPheobe: function () {
+        console.log(`You played with ${this.name}! Happiness is now ${happiness + this.happiness}!`);
+        this.happiness += 10;
+    },
+
+    feedPheobe: function(){
+
+    },
+
+    status: function () {
+console.log(`Phoebe Status...Name: ${ths.name}, Species: ${this.species}, Happiness: ${this.happiness}`);
+    }
+
+}
+
+
 
 console.log(pet.name);
 console.log(pet.species);
