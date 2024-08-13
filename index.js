59d1d48a63bba45d6dbe09aa573545557923fe2d
trincellshelvin <trincellshelvin@gmail.com>
2024-08-13 03:44:33 +0000

    created day1 index.js with pet object

---
+++ index.js
@@ -0,0 +1,11 @@
+const pet ={
+    name: 'Pheobe Cinnamon Sunrise',
+    species: 'Dog',
+    breed: 'Miniture Pitbull Terrier',
+    happiness: 50
+};
+
+console.log(pet.name);
+console.log(pet.species);
+console.log(pet.breed);
+console.log(pet.happiness);
